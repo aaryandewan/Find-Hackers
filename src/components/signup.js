f5c9b28387e56094b6032a84e1648066c768e244
Aaryan Dewan <aaryandewan@yahoo.com>
2022-08-21 13:48:01 +0530

    signup functionality working

--- src/components/signup.js
+++ src/components/signup.js
@@ -4,7 +4,6 @@ import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Link from "@mui/material/Link";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
@@ -12,6 +11,10 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { useNavigate } from "react-router-dom";
+
+//firebase imports
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 function Copyright(props) {
   return (
@@ -34,9 +37,21 @@ function Copyright(props) {
 const theme = createTheme();
 
 export default function SignUp() {
-  const handleSubmit = (event) => {
+  let navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
+
+    //changed:
+    const auth = getAuth();
+    await createUserWithEmailAndPassword(
+      auth,
+      data.get("email"),
+      data.get("password")
+    );
+    navigate("/");
+
     console.log({
       email: data.get("email"),
       password: data.get("password"),
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,15 +1,19 @@
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
+const auth = getAuth();
+
 export function signUpUserWithEmailAndPassword(auth, email, password) {
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       // Signed in
       const user = userCredential.user;
+      console.log("User signed up (in services), user = ", user);
       // ...
     })
     .catch((error) => {
       const errorCode = error.code;
       const errorMessage = error.message;
+      console.error("Cannot sign up user", errorCode, errorMessage);
       // ..
     });
 }
