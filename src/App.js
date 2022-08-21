7b2675b157b8f90540fca186bbf523e2b3495286
Aaryan Dewan <aaryandewan@yahoo.com>
2022-08-21 11:42:40 +0530

    Set up basic routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import Button from "@mui/material/Button";
-import { useContext } from "react";
+import { lazy, Suspense } from "react";
+import * as ROUTES from "./constants/routes";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import FirebaseContext from "./context/firebase";
+const Signup = lazy(() => import("./pages/signup"));
+const Login = lazy(() => import("./pages/login"));
+const Dashboard = lazy(() => import("./pages/dashboard"));
+const NotFound = lazy(() => import("./pages/not-found"));
+
 export default function App() {
-  const { firebaseApp } = useContext(FirebaseContext);
+  return (
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Routes>
+          <Route path={ROUTES.DASHBOARD} element={<Dashboard />}></Route>
 
-  console.log("FirebaseApp", firebaseApp);
-
-  return <Button variant="contained">Hello World</Button>;
+          <Route path={ROUTES.LOG_IN} element={<Login />}></Route>
+          <Route path={ROUTES.SIGN_UP} element={<Signup />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Routes>
+      </Suspense>
+    </Router>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import FirebaseContext from "./context/firebase";
-import { firebaseApp } from "./firebase.config";
+import { firebaseApp, auth } from "./firebase.config";
 
 // console.log("index.js", firebaseApp);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <FirebaseContext.Provider value={{ firebaseApp }}>
+    <FirebaseContext.Provider value={{ firebaseApp, auth }}>
       <App />
     </FirebaseContext.Provider>
   </React.StrictMode>
